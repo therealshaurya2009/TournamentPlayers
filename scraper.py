35822bdaeca2ce629eb3043fa053c940654d5956
therealshaurya2009 <135183763+therealshaurya2009@users.noreply.github.com>
2025-08-04 18:04:07 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -33,18 +33,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def setup_driver():
-    driver_options = Options()
-    driver_options.add_argument("--headless=new")
-    driver_options.add_argument("--disable-gpu")
-    driver_options.add_argument("--window-size=1920,1080")
-    driver_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
-    driver_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    driver_options.add_experimental_option('useAutomationExtension', False)
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=driver_options)
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    options = Options()
+    options.binary_location = "/usr/bin/google-chrome"
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    
+    service = Service("/usr/local/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
-
 
 def age_groups_level(tournament_link):
     driver = setup_driver()
